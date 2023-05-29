dd314727a197eff3f2f46ab17671da2864feecd4
Rabin294 <bloomworld294@yahoo.com>
2023-05-29 20:33:08 +0900

    added CSRF Protection in the system

--- register.php
+++ register.php
@@ -1,35 +1,42 @@
 <?php
 require_once 'core/init.php';
 
+
+
 if(Input::exists()){
-    $validate = new Validate();
-    $validation = $validate->check($_POST, array(
-        'username'=> array(
-            'required'=> true,
-            'min'=>2,
-            'max'=>20,
-            'unique'=>'users'
-        ),
-        'password'=> array(
-            'required'=>true,
-            'min'=>6
-        ),
-        'password_again'=> array(
-            'required'=>true,
-            'matches'=>'password'
-        ),
-        'name'=>array(
-            'required'=> true,
-            'min'=>2,
-            'max'=>50
-        )
-    ));
-            if($validation->passed()){
-                echo 'Passed';
-            }else{
-                print_r($validation->errors());
-            }
+    if(Token::check(Input::get('token'))){
+
+        $validate = new Validate();
+        $validation = $validate->check($_POST, array(
+            'username'=> array(
+                'required'=> true,
+                'min'=>2,
+                'max'=>20,
+                'unique'=>'users'
+            ),
+            'password'=> array(
+                'required'=>true,
+                'min'=>6
+            ),
+            'password_again'=> array(
+                'required'=>true,
+                'matches'=>'password'
+            ),
+            'name'=>array(
+                'required'=> true,
+                'min'=>2,
+                'max'=>50
+            )
+        ));
+                if($validation->passed()){
+                    echo 'Passed';
+                }else{
+                    foreach($validation->errors() as $error){
+                        echo $error, '<br>';
+                    }
+                }
    
+    }
 }
 ?>
  
@@ -50,6 +57,7 @@ if(Input::exists()){
         <label for="full_name">Enter your full name</label>
         <input type="text" name="name" id="name" value="<?php echo escape(Input::get('name'))?>" >
     </div>
+    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
     <input type="submit" value="Register">
 
  </form>
